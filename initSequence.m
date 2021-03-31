14bf341e938f0712e736aff4f11dfb0bf98178e6
Ryan Thomas <ryan.j.thomas1@gmail.com>
2021-04-01 09:04:22 +1100

    initSequence now defines defaults for all channels

--- initSequence.m
+++ initSequence.m
@@ -10,7 +10,7 @@ function sq = initSequence
     sq.channels(5).setName('25W Active','A4').setDefault(1);
     sq.channels(6).setName('A5 - N/C','A5').setDefault(0);
     sq.channels(7).setName('Bragg/Raman switch','A6','Set to 1 for Bragg, 0 for Raman').setDefault(0);
-    sq.channels(8).setName('A7 - N/C','A7');
+    sq.channels(8).setName('A7 - N/C','A7').setDefault(0);
     sq.channels(9).setName('2D MOT Amp TTL','B0').setDefault(1);
     sq.channels(10).setName('3D MOT Amp TTL','B1').setDefault(1);
     sq.channels(11).setName('Imaging Amp TTL','B2').setDefault(0);
@@ -21,7 +21,12 @@ function sq = initSequence
     sq.channels(16).setName('Fiber Switch Repump','B7').setDefault(0);
     sq.channels(17).setName('MOT Coil TTL','C0').setDefault(1);
     sq.channels(18).setName('Stern-Gerlach Trigger','C1').setDefault(0);
-
+    sq.channels(19).setName('C2 - N/C','C2').setDefault(0);
+    sq.channels(20).setName('C3 - N/C','C3').setDefault(0);
+    sq.channels(21).setName('C4 - N/C','C4').setDefault(0);
+    sq.channels(22).setName('C5 - N/C','C5').setDefault(0);
+    sq.channels(23).setName('C6 - N/C','C6').setDefault(0);
+    sq.channels(24).setName('C7 - N/C','C7').setDefault(0);
     sq.channels(25).setName('MW Amp TTL','D0').setDefault(0);
     sq.channels(26).setName('F MOD Imaging Trigger','D1').setDefault(0);
     sq.channels(30).setName('Drop 1 Camera Trig','D5').setDefault(0);
@@ -47,7 +52,7 @@ function sq = initSequence
     sq.analog(15).setName('Drop Repump Freq','BO/6').setDefault(4.3);
     sq.analog(16).setName('RF Frequency','BO/7').setDefault(0);
     sq.analog(17).setName('Raman TTL','CO/0').setDefault(0);
-    sq.analog(18).setName('CO/1 - dead','CO/1');
+    sq.analog(18).setName('CO/1 - dead','CO/1').setDefault(0);
     sq.analog(19).setName('3D Coils','CO/2').setDefault(0.42);
     sq.analog(20).setName('Bias E/W','CO/3').setDefault(0);
     sq.analog(21).setName('Bias N/S','CO/4').setDefault(0);
@@ -56,8 +61,8 @@ function sq = initSequence
     sq.analog(24).setName('3D Coils Loop','CO/7').setDefault(0);
 
     %% DDS default
-%     sq.dds(1).setName('DDS 1').setDefault([110,0,0]);
-%     sq.dds(2).setName('DDS 2').setDefault([110,0,0]);
+    sq.dds(1).setName('DDS 1').setDefault([110,0,0]);
+    sq.dds(2).setName('DDS 2').setDefault([110,0,0]);
     
 
 end
